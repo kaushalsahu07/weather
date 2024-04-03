1f63c56496160a8aa05714379f2fe84ac67784e4
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-04-04 00:13:33 +0530

    .

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let apiKey = '1e3e8f230b6064d27976e41163a82b77';
+let url =  `https://api.openweathermap.org/data/2.5/weather?`;
+
+async function loc(){
+    let respond = await fetch(url + `lat=${lat}&lon=${lon}&` + `appid=${apiKey}`)
+    let data = await respond.json();
+    console.log(data)
+}
+
+loc()
