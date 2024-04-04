a67b2da25c96baa35d7ceac708449a4b5498991a
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-04-04 13:43:24 +0530

    Change API key variable name to string, update function to use geolocation
    * Changed `apiKey` variable to string syntax
    * Updated `loc()` function to use `navigator.geolocation.getCurrentPosition()` to get user's current location and pass latitude and longitude to the API request URL.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let apiKey = '1e3e8f230b6064d27976e41163a82b77';
-let url =  `https://api.openweathermap.org/data/2.5/weather?`;
+let apiKey = "1e3e8f230b6064d27976e41163a82b77";
+let url = `https://api.openweathermap.org/data/2.5/weather?`;
+let 
 
-async function loc(){
-    let respond = await fetch(url + `lat=${lat}&lon=${lon}&` + `appid=${apiKey}`)
-    let data = await respond.json();
-    console.log(data)
-}
+navigator.geolocation.getCurrentPosition(async function (position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  let respond = await fetch(url + `lat=${lat}&lon=${lon}&` + `appid=${apiKey}`);
+  let data = await respond.json();
 
-loc()
+  console.log(data);
+});
