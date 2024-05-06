78410c970093b09055d9983810a17ad30f886e02
kaushalsahu07 <kaushal.r.sahu@gmail.com>
2024-05-06 23:55:30 +0530

    "Add search functionality and error handling for city weather display; update CSS for add-section"
    This commit includes the following changes:
    
    * Adds a search input field to the add-section, along with a search icon and messages

--- js/world.js
+++ js/world.js
@@ -1,4 +1,9 @@
 let apiKey = "1e3e8f230b6064d27976e41163a82b77";
+let searchinput = document.querySelector(".searchinput");
+let box = document.querySelector(".box");
+let normalMessage = document.querySelector(".normal-message");
+let errorMessage = document.querySelector(".error-message");
+let addedMessage = document.querySelector(".added-message");
 
 // Function to get the date
 let date = new Date().getDate();
@@ -27,85 +32,108 @@ async function city(cityName) {
   let url = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${apiKey}`
   );
-  let data = await url.json();
-  console.log(data);
+  if (url.ok) {
+    let data = await url.json();
+    console.log(data);
 
-  let box = document.querySelector(".box");
-  let pcsreen = document.querySelector(".pc");
-  
-  if (!box) {
-    box = document.createElement("div");
-    box.className = "box";
-    pcsreen.appendChild(box);
-  }
+    let pcsreen = document.querySelector(".pc");
 
-  let weatherBox = document.createElement("div");
-  weatherBox.className = "weather-box";
-
-  let nameDiv = document.createElement("div");
-  nameDiv.className = "name";
-
-  let cityElement = document.createElement("div");
-  cityElement.className = "city-name city";
-  cityElement.innerHTML = data.name;
-
-  let tempElement = document.createElement("div");
-  tempElement.className = "weather-temp temp";
-  tempElement.innerHTML = Math.floor(data.main.temp) + "°";
-
-  let weatherIconDiv = document.createElement("div");
-  weatherIconDiv.className = "weather-icon";
-
-  let weatherImg = document.createElement("img");
-  weatherImg.className = "weather";
-
-  if (data.weather[0].main === "Rain") {
-    weatherImg.src = "img/rain.png";
-  } else if (data.weather[0].main === "Clear") {
-    weatherImg.src = "img/sun.png";
-  } else if (data.weather[0].main === "Snow") {
-    weatherImg.src = "img/snow.png";
-  } else if (
-    data.weather[0].main === "Clouds" ||
-    data.weather[0].main === "Smoke"
-  ) {
-    weatherImg.src = "img/cloud.png";
-  } else if (
-    data.weather[0].main === "Mist" ||
-    data.weather[0].main === "Fog"
-  ) {
-    weatherImg.src = "img/mist.png";
-  } else if (data.weather[0].main === "Haze") {
-    weatherImg.src = "img/haze.png";
-  }
-
-  weatherIconDiv.appendChild(weatherImg);
-  nameDiv.appendChild(cityElement);
-  nameDiv.appendChild(tempElement);
-  weatherBox.appendChild(nameDiv);
-  weatherBox.appendChild(weatherIconDiv);
-  box.appendChild(weatherBox);
-
-
-  // add section
-  let section = document.querySelector(".add-section");
-  let navBtn = document.querySelector(".button");
-  let navIcon = document.querySelector(".btn-icon")
-
-  navBtn.addEventListener("click", () => {
-    if(section.style.top === "-60rem" ){
-      section.style.top = "20px";
-      navIcon.className = "fa-solid fa-circle-xmark"
-    }else{
-      section.style.top = "-60rem"
-      navIcon.className = "fa-solid fa-circle-plus"
+    if (!box) {
+      box = document.createElement("div");
+      box.className = "box";
+      pcsreen.appendChild(box);
     }
-  });
 
+    let weatherBox = document.createElement("div");
+    weatherBox.className = "weather-box";
+
+    let nameDiv = document.createElement("div");
+    nameDiv.className = "name";
+
+    let cityElement = document.createElement("div");
+    cityElement.className = "city-name city";
+    cityElement.innerHTML = data.name;
+
+    let tempElement = document.createElement("div");
+    tempElement.className = "weather-temp temp";
+    tempElement.innerHTML = Math.floor(data.main.temp) + "°";
+
+    let weatherIconDiv = document.createElement("div");
+    weatherIconDiv.className = "weather-icon";
+
+    let weatherImg = document.createElement("img");
+    weatherImg.className = "weather";
+
+    if (data.weather[0].main === "Rain") {
+      weatherImg.src = "img/rain.png";
+    } else if (data.weather[0].main === "Clear") {
+      weatherImg.src = "img/sun.png";
+    } else if (data.weather[0].main === "Snow") {
+      weatherImg.src = "img/snow.png";
+    } else if (
+      data.weather[0].main === "Clouds" ||
+      data.weather[0].main === "Smoke"
+    ) {
+      weatherImg.src = "img/cloud.png";
+    } else if (
+      data.weather[0].main === "Mist" ||
+      data.weather[0].main === "Fog"
+    ) {
+      weatherImg.src = "img/mist.png";
+    } else if (data.weather[0].main === "Haze") {
+      weatherImg.src = "img/haze.png";
+    }
+
+    weatherIconDiv.appendChild(weatherImg);
+    nameDiv.appendChild(cityElement);
+    nameDiv.appendChild(tempElement);
+    weatherBox.appendChild(nameDiv);
+    weatherBox.appendChild(weatherIconDiv);
+    box.appendChild(weatherBox);
+
+    return weatherBox;
+  } else {
+   
+    normalMessage.style.display = "none";
+    errorMessage.style.display = "block";
+    addedMessage.style.display = "none";
+    return "";
+  }
 }
+
+// add section
+let section = document.querySelector(".add-section");
+let navBtn = document.querySelector(".button");
+let navIcon = document.querySelector(".btn-icon");
+
+navBtn.addEventListener("click", () => {
+  if (section.style.top === "-60rem") {
+    section.style.top = "100px";
+    navIcon.className = "fa-solid fa-circle-xmark";
+  } else {
+    section.style.top = "-60rem";
+    navIcon.className = "fa-solid fa-circle-plus";
+  }
+});
+
+let worked = false;
+
+searchinput.addEventListener("keydown", async function (event) {
+  if (event.keyCode === 13 || event.which === 13) {
+    const weatherInfo = await city(searchinput.value);
+    if (weatherInfo) {
+      worked = true; // Set the flag to true if weatherInfo is not empty
+    }
+    box.prepend(weatherInfo);
+    if (worked) {
+      normalMessage.style.display = "none";
+      errorMessage.style.display = "none";
+      addedMessage.style.display = "block";
+    } 
+
+  }
+});
 
 city("London");
 city("Paris");
 city("Mumbai");
-city("Tokyo");
-city("Rome");
